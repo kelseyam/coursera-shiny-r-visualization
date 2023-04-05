7773b663866cc1fc93b685e57dcaa800288de74f
kelseyam <128943819+kelseyam@users.noreply.github.com>
2023-04-05 15:11:31 -0400

    Update server.r

--- server.r
+++ server.r
@@ -16,22 +16,23 @@ shinyServer(function(input, output) {
   
   # TASK 5: Create logic to plot histogram or boxplot
   output$p1 <- renderPlot({
+    data <- read.csv("adult.csv")
     if (input$graph_type == "histogram") {
       # Histogram
-      ggplot(df_country, aes_string(x= "input$radio_continuous")) +
+      ggplot(df_country(), aes_string(x=input$radio_continuous)) +
         geom_histogram(bins = 30) +  # histogram geom
-        labs(title = paste("Trend of",input$radio_continuous)) +
-        ylab("Number of People") + # labels
+        labs(title = paste("Trend of",input$radio_continuous),
+             y = "Number of People") +# labels
         facet_wrap(~prediction)    # facet by prediction
     }
     else {
       # Boxplot
-      ggplot(df_country, aes_string(y = "input$radio_continuous")) +
+      ggplot(df_country(), aes_string(y =input$radio_continuous)) +
         geom_boxplot() +  # boxplot geom
         coord_flip() +  # flip coordinates
-        labs(title = paste("How", input$radio_continuous,"value is spread")) +
-        ylab("Number of People")+
-      xlabs("Age") +  # labels
+        labs(title = paste("How", input$radio_continuous,"value is spread"),
+             y = "Number of People",
+             x = "Age") +  # labels
         facet_wrap(~prediction)    # facet by prediction
     }
     
@@ -40,14 +41,16 @@ shinyServer(function(input, output) {
   # TASK 6: Create logic to plot faceted bar chart or stacked bar chart
   output$p2 <- renderPlot({
     # Bar chart
-    p2 <- ggplot(df_country, aes_string(x = "input$radio_categorical")) +
-      geom_bar() +
-      labs(title = paste("Trend of", input$radio_categorical), x = paste("Sorted by", input$radio_categorical), y = "Number of People", legend(color = factor(categorical_variables)))+# labels
+    p2 <- ggplot(df_country(), aes_string(x =input$radio_categorical)) +
+      geom_bar(aes(color = 'categorical_variables')) +
+      labs(title = paste("Trend of", input$radio_categorical),
+           x = "Categorical Variable",
+           y = 'Number of People')+# labels
       theme(axis.text.x.bottom = element_text(angle = 45),
             legend.position = "bottom") # modify theme to change text angle and legend position
     
     if (input$is_stacked) {
-      p2 + geom_bar(aes(fill = "prediction"))
+      p2 + geom_bar(aes(fill = prediction))
     }  # add bar geom and use prediction as fill
     
     else{
